fc9d9ca9b56f95f94ef46afeb57435baf3752ec7
DmitriyPodpryatov <podp2000@gmail.com>
2021-03-01 10:16:21 +0300

    Solve 5.1 and 5.2 for single day

--- features.py
+++ features.py
@@ -4,29 +4,42 @@ from utils import instruments_info
 
 
 class FeatureGenerator(Spectrum):
-    instruments = [
-        'USD000000TOM',
-        'USD000UTSTOM',
-        'EUR_RUB__TOD',
-        'EUR_RUB__TOM'
-    ]
+
+    BEST_BID = -1
+    BEST_ASK = int(1E19)
 
     MAX_BAND = 10 * 1E6
 
-    BAND_VALUES = [
-             1E5,
-        2  * 1E5,
-        5  * 1E5,
-             1E6,
-        5  * 1E6,
-        10 * 1E6
-    ]
+    BAND_VALUES = list(map(int, [     1E5,
+                                 2  * 1E5,
+                                 5  * 1E5,
+                                      1E6,
+                                 5  * 1E6,
+                                 10 * 1E6]
+                           ))
 
-    def __init__(self, seccode):
-        super().__init__(seccode)
+    def __init__(self, seccode, px_step):
+        self.seccode = seccode
+        self.px_step = px_step
+        self.best_bid = self.BEST_BID
+        self.best_ask = self.BEST_ASK
+        self.bids = [0] * 10
+        self.asks = [0] * 10
+        self.bids_normalized = [0] * 10
+        self.asks_normalized = [0] * 10
 
         self.VWAP_bids = { band: 0 for band in self.BAND_VALUES }
         self.VWAP_asks = { band: 0 for band in self.BAND_VALUES }
+        self.VWAP_bids_normalized = { band: 0 for band in self.BAND_VALUES }
+        self.VWAP_asks_normalized = { band: 0 for band in self.BAND_VALUES }
+
+    def reset_VWAP_bids(self):
+        self.VWAP_bids = { band: 0 for band in self.BAND_VALUES }
+        self.VWAP_bids_normalized = { band: 0 for band in self.BAND_VALUES }
+
+    def reset_VWAP_asks(self):
+        self.VWAP_asks = { band: 0 for band in self.BAND_VALUES }
+        self.VWAP_asks_normalized = { band: 0 for band in self.BAND_VALUES }
 
     def normalize(self):
         """
@@ -34,39 +47,118 @@ class FeatureGenerator(Spectrum):
         """
 
         self.bids_normalized = [ bid / self.MAX_BAND for bid in self.bids ]
-
         self.asks_normalized = [ ask / self.MAX_BAND for ask in self.asks ]
-
-        self.normalize_VWAPs()
 
     def normalize_VWAPs(self):
         """
         Normalize VWAP relative to midpoint
         """
 
-        mid_px = (self.best_bid + self.best_ask) / 2
+        if self.best_bid == self.BEST_BID:
+            mid_px = self.best_ask
+        elif self.best_ask == self.BEST_ASK:
+            mid_px = self.best_bid
+        else:
+            mid_px = (self.best_bid + self.best_ask) / 2
 
-        px_step = self.get_px_step()
+        self.VWAP_bids_normalized = { band: (mid_px - VWAP_bid) / self.px_step
+                           for band, VWAP_bid in self.VWAP_bids.items() }
 
-        for band, VWAP_bid in self.VWAP_bids.items():
-            self.VWAP_bids[band] = (mid_px - VWAP_bid) / px_step
+        self.VWAP_asks_normalized = { band: (VWAP_ask - mid_px) / self.px_step
+                           for band, VWAP_ask in self.VWAP_asks.items() }
 
-        for band, VWAP_ask in self.VWAP_asks.items():
-            self.VWAP_asks[band] = (VWAP_ask - mid_px) / px_step
+    def update_VWAP_bids(self, order_book: OrderBook):
+        # Reset VWAP bids before updating them
+        self.reset_VWAP_bids()
 
-    def get_px_step(self):
-        return instruments_info[self.seccode]['PRICE_STEP']
+        # Handle empty bids
+        if not order_book.bids:
+            return
 
-    def change_VWAP_bids(self, order_book: OrderBook):
-        # TODO: recalculate VWAP bids
-        pass
+        # Sum of (price * volume), Covered volume
+        total, volume = 0, 0
+        # Index and value of current band
+        band_idx = 0
+        band = self.BAND_VALUES[band_idx]
 
-    def change_VWAP_asks(self, order_book: OrderBook):
-        # TODO: recalculate VWAP asks
-        pass
+        # Iterate through bids
+        for bid in order_book.bids.values():
+            current_volume = bid['VOLUME']
+
+            # Update band if new volume is greater than current band
+            while volume + current_volume >= band:
+                # Update current volume
+                current_volume = volume + current_volume - band
+
+                # Update VWAP
+                total += bid['PRICE'] * (band - volume)
+                volume = band
+                self.VWAP_bids[band] = total / band
+
+                # Update band
+                try:
+                    band_idx += 1
+                    band = self.BAND_VALUES[band_idx]
+                except IndexError:
+                    break
+            else:
+                # Update total price and covered volume
+                total += bid['PRICE'] * current_volume
+                volume += current_volume
+
+        # Handle remaining bands
+        if band_idx < len(self.BAND_VALUES):
+            for b in self.BAND_VALUES:
+                if b >= band:
+                    self.VWAP_bids[b] = total / volume
+
+    def update_VWAP_asks(self, order_book: OrderBook):
+        # Reset VWAP asks before updating them
+        self.reset_VWAP_asks()
+
+        # Handle empty asks
+        if not order_book.asks:
+            return
+
+        # Sum of (price * volume), Covered volume
+        total, volume = 0, 0
+        # Index and value of current band
+        band_idx = 0
+        band = self.BAND_VALUES[band_idx]
+
+        # Iterate through asks
+        for bid in order_book.asks.values():
+            current_volume = bid['VOLUME']
+
+            # Update band if new volume is greater than current band
+            while volume + current_volume >= band:
+                # Update current volume
+                current_volume = volume + current_volume - band
+
+                # Update VWAP
+                total += bid['PRICE'] * (band - volume)
+                volume = band
+                self.VWAP_asks[band] = total / band
+
+                # Update band
+                try:
+                    band_idx += 1
+                    band = self.BAND_VALUES[band_idx]
+                except IndexError:
+                    break
+            else:
+                # Update total price and covered volume
+                total += bid['PRICE'] * current_volume
+                volume += current_volume
+
+        # Handle remaining bands
+        if band_idx < len(self.BAND_VALUES):
+            for b in self.BAND_VALUES:
+                if b >= band:
+                    self.VWAP_asks[b] = total / volume
 
     def update_post(self, order_book: OrderBook, new_price: float, volume: int, ask: bool):
-        step = self.get_px_step()
+        step = self.px_step
 
         if not ask:
             if new_price > self.best_bid:
@@ -83,7 +175,7 @@ class FeatureGenerator(Spectrum):
             else:
                 self.change_bids(price=new_price, volume=volume, step=step, add=True)
 
-            self.change_VWAP_bids(order_book)
+            self.update_VWAP_bids(order_book)
 
         else:
             if new_price < self.best_ask:
@@ -100,18 +192,19 @@ class FeatureGenerator(Spectrum):
             else:
                 self.change_asks(price=new_price, volume=volume, step=step, add=True)
 
-            self.change_VWAP_asks(order_book)
+            self.update_VWAP_asks(order_book)
 
         self.normalize()
+        self.normalize_VWAPs()
 
     def update_revoke(self, order_book: OrderBook, new_price: float, volume: int, ask: bool):
-        step = self.get_px_step()
+        step = self.px_step
 
         if not ask:
             if new_price == self.best_bid:
                 if volume >= self.bids[9]:
 
-                    self.best_bid = -1
+                    self.best_bid = self.BEST_BID
 
                     for bid in order_book.bids.values():
                         if bid['PRICE'] > self.best_bid:
@@ -127,13 +220,13 @@ class FeatureGenerator(Spectrum):
             elif new_price < self.best_bid:
                 self.change_bids(price=new_price, volume=volume, step=step, add=False)
 
-            self.change_VWAP_bids(order_book)
+            self.update_VWAP_bids(order_book)
 
         else:
             if new_price == self.best_ask:
                 if volume >= self.asks[0]:
 
-                    self.best_ask = 10000000000000000000
+                    self.best_ask = self.BEST_ASK
 
                     for ask in order_book.asks.values():
                         if ask['PRICE'] < self.best_ask:
@@ -150,6 +243,7 @@ class FeatureGenerator(Spectrum):
             elif new_price > self.best_ask:
                 self.change_asks(price=new_price, volume=volume, step=step, add=False)
 
-            self.change_VWAP_asks(order_book)
+            self.update_VWAP_asks(order_book)
 
         self.normalize()
+        self.normalize_VWAPs()
